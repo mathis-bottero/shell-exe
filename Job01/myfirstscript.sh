26fc59bbfcfd72e85055acd57d5819fc022c15e4
mathis-bottero <mathis.bottero@laplateforme.io>
2023-10-13 15:00:48 +0200

    shell-exe

---
+++ Job01/myfirstscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "I’m a script"
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
---
+++ Job03/add.sh
@@ -0,0 +1,6 @@
+nombre1="1"
+
+nombre2="2"
+total=$((nombre1 + nombre2))
+
+echo "result : $total"
